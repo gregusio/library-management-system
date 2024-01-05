0fbd43451a0820ae4a380a79c6d893a9bf770b90
gregusio <grzegorzdynaq@gmail.com>
2024-01-05 20:28:03 +0100

    add database management classes

---
+++ Database/RemoveDb.cs
@@ -0,0 +1,36 @@
+namespace Database;
+
+public class RemoveDb
+{
+    private readonly Data _db = new Data();
+    
+    public void RemoveReader(int id)
+    {
+        var reader = _db.Readers.FirstOrDefault(reader => reader.Id == id);
+        if (reader != null)
+        {
+            _db.Readers.Remove(reader);
+            _db.SaveChanges();
+        }
+    }
+    
+    public void RemoveLibrarian(int id)
+    {
+        var librarian = _db.Librarians.FirstOrDefault(librarian => librarian.Id == id);
+        if (librarian != null)
+        {
+            _db.Librarians.Remove(librarian);
+            _db.SaveChanges();
+        }
+    }
+    
+    public void RemoveBook(int id)
+    {
+        var book = _db.Books.FirstOrDefault(book => book.BookId == id);
+        if (book != null)
+        {
+            _db.Books.Remove(book);
+            _db.SaveChanges();
+        }
+    }
+}
---
+++ Database/SearchDb.cs
@@ -0,0 +1,36 @@
+namespace Database;
+
+public class SearchDb
+{
+    private readonly Data _db = new Data();
+    
+    public Reader? GetReader(int id)
+    {
+        return _db.Readers.FirstOrDefault(reader => reader.Id == id);
+    }
+    
+    public Reader? GetReader(string login)
+    {
+        return _db.Readers.FirstOrDefault(reader => reader.Login == login);
+    }
+    
+    public Librarian? GetLibrarian(int id)
+    {
+        return _db.Librarians.FirstOrDefault(librarian => librarian.Id == id);
+    }
+    
+    public Librarian? GetLibrarian(string login)
+    {
+        return _db.Librarians.FirstOrDefault(librarian => librarian.Login == login);
+    }
+    
+    public List<Book> GetBooks(string title, string? author)
+    {
+        var books = _db.Books.Where(book => book.info.Title == title);
+        if (author != null)
+        {
+            books = books.Where(book => book.info.Author == author);
+        }
+        return books.ToList();
+    }
+}
