b2868f946b69d205ae629ab75e0fc617aaf43270
Grzegorz Dynak <77176069+gregusio@users.noreply.github.com>
2024-01-10 20:41:25 +0100

    Basic login and navigation menu (#2)
    * login page
    
    * changes
    
    * change file
    
    * icon fix
    
    * add login and password format check
    
    * add AuthService
    
    * add logged user check
    
    * add logout site
    
    * login example
    
    * add AuthService
    
    * change structure
    
    * add nav menu factory
    
    * change page names
    
    * add new pages
    
    * add nav service
    
    * change nav
    
    * change sites content
    
    ---------
    
    Co-authored-by: Dora455 <dyczek02@gmail.com>

---
+++ Components/Layout/DefaultNavigationMenu.cs
@@ -0,0 +1,12 @@
+namespace library_management_system.Components.Layout;
+
+public class DefaultNavigationMenu : INavigationMenu
+{
+    public List<(string, string)> GetItems()
+    {
+        return
+        [
+            ("", "Login"),
+        ];
+    }
+}
