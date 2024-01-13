92b1c381802c64bf31944243c20c939f6b20ca36
gregusio <grzegorzdynaq@gmail.com>
2024-01-13 16:21:19 +0100

    change DbApi service from singleton to transient

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-builder.Services.AddSingleton<DbApi>();
+builder.Services.AddTransient<DbApi>();
 
 builder.Services.AddSingleton<AuthService>();
 
