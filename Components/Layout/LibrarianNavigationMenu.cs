f23779c4a486e54f4742c23b373c76b0f119e739
Grzegorz Dynak <grzegorzdynaq@gmail.com>
2024-01-18 01:44:26 +0100

    add icons to navigation

--- Components/Layout/LibrarianNavigationMenu.cs
+++ Components/Layout/LibrarianNavigationMenu.cs
@@ -1,17 +1,19 @@
+using BlazorBootstrap;
+
 namespace library_management_system.Components.Layout;
 
 public class LibrarianNavigationMenu : INavigationMenu
 {
-    public List<(string, string)> GetItems()
+    public List<(string, string, IconName)> GetItems()
     {
         return
         [
-            ("books", "Books"),
-            ("readers", "Readers"),
-            ("borrow-book", "Borrow book"),
-            ("return-book", "Return book"),
-            ("librarians", "Librarians"),
-            ("logout", "Logout")
+            ("books", "Books", IconName.Book),
+            ("readers", "Readers", IconName.People),
+            ("borrow-book", "Borrow book", IconName.Book),
+            ("return-book", "Return book", IconName.Book),
+            ("librarians", "Librarians", IconName.People),
+            ("logout", "Logout", IconName.ArrowLeftSquareFill)
         ];
     }
 }
