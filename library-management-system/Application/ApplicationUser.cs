cc665479b15b1261e7fa6d10074f597db3898189
gregusio <grzegorzdynaq@gmail.com>
2024-03-22 23:50:29 +0100

    add basic new login from template

---
+++ library-management-system/Application/ApplicationUser.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace library_management_system.Application
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+
+}
