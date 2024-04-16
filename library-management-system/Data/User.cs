5a85b235c54766a17ef2494f35375c815bed8eeb
gregusio <grzegorzdynaq@gmail.com>
2024-04-16 21:49:40 +0200

    some change

--- library-management-system/Data/User.cs
+++ library-management-system/Data/User.cs
@@ -5,8 +5,6 @@ namespace library_management_system.Data;
 
 public class User : IdentityUser
 {
-    [Key] public int Id { get; set; }
-
     [Required, StringLength(30)] public string? Name { get; set; }
 
     [Required, StringLength(30)] public string? Surname { get; set; }
@@ -17,25 +15,19 @@ public class User : IdentityUser
     [StringLength(30)]
     public string? TelephoneNr { get; set; }
 
-    [EmailAddress, StringLength(30)] public string? EmailAddress { get; set; }
-
-    [Required, StringLength(30)] public string? Login { get; set; }
-
-    [Required, StringLength(30)] public string? Hash { get; set; }
+    [Required, StringLength(30)]
+    public string? Role { get; set; }
     
     public User()
     {
     }
 
-    public User(int id, string? name, string? surname, string? address, string? telephoneNr, string? emailAddress, string? login, string? hash)
+    public User(string name, string surname, string address, string telephoneNr, string role)
     {
-        Id = id;
         Name = name;
         Surname = surname;
         Address = address;
         TelephoneNr = telephoneNr;
-        EmailAddress = emailAddress;
-        Login = login;
-        Hash = hash;
+        Role = role;
     }
 }
