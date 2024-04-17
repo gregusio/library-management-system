2bda4741cf426689ff39c1f8865fe1f8fa2a8a9e
gregusio <grzegorzdynaq@gmail.com>
2024-04-18 00:31:47 +0200

    remove useless functions and change some others

--- library-management-system/Data/SearchDb.cs
+++ library-management-system/Data/SearchDb.cs
@@ -4,45 +4,14 @@ namespace library_management_system.Data;
 
 public class SearchDb(DataDbContext db)
 {
-    public User? GetReader(int id)
-    {
-        // return db.Users.FirstOrDefault(reader => reader.Id == id);
-        return null;
-    }
-
-    public User? GetReader(string login)
-    {
-        // return db.Users.FirstOrDefault(reader => reader.Login == login);
-        return null;
-    }
-
     public List<User> GetAllReaders()
     {
-        return db.Users.ToList();
-    }
-
-    public User? GetLibrarian(int id)
-    {
-        // return db.Users.FirstOrDefault(librarian => librarian.Id == id);
-        return null;
-    }
-
-    public User? GetLibrarian(string login)
-    {
-        // return db.Users.FirstOrDefault(librarian => librarian.Login == login);
-        return null;
+        return db.Users.Where(user => user.Role == ERole.Reader).ToList();
     }
 
     public List<User> GetAllLibrarians()
     {
-        return db.Users.ToList();
-    }
-
-    public List<Book> GetBooks(string title, string? author)
-    {
-        var books = db.Books.Where(book => book.Title == title);
-        if (author != null) books = books.Where(book => book.Author == author);
-        return books.ToList();
+        return db.Users.Where(user => user.Role == ERole.Librarian).ToList();
     }
 
     public List<Book> GetAllBooks()
@@ -69,20 +38,5 @@ public class SearchDb(DataDbContext db)
     {
         return db.ReservedBooks.FirstOrDefault(reservedBook =>
             reservedBook.ReaderId == reader.Id && reservedBook.BookId == book.BookId);
-    }
-    
-    public bool LoginAlreadyUsed(string login)
-    {
-        // return db.Users.Any(r => r.Login == login) || db.Users.Any(l => l.Login == login);
-        return true;
-    }
-    
-    public bool PasswordAlreadyUsed(string password)
-    {
-        // var passwordHasher = new PasswordHasher<User>();
-        // var passwordHash = passwordHasher.HashPassword(null!, password);
-        // return db.Users.Any(r => r.Hash == passwordHash)
-        //        || db.Users.Any(l => l.Hash == passwordHash);
-        return true;
     }
 }
