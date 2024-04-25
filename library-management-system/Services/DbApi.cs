e015c3970cb3636270ccc0225abe96ec365e07cc
gregusio <grzegorzdynaq@gmail.com>
2024-04-25 22:42:25 +0200

    db api changes according to previous changes

--- library-management-system/Services/DbApi.cs
+++ library-management-system/Services/DbApi.cs
@@ -6,81 +6,91 @@ public class DbApi(DataDbContext db)
 {
     public List<User> GetAllReaders()
     {
-        return new SearchDb(db).GetAllReaders();
+        return new DbSearchService(db).GetAllReaders();
     }
 
     public List<User> GetAllLibrarians()
     {
-        return new SearchDb(db).GetAllLibrarians();
+        return new DbSearchService(db).GetAllLibrarians();
     }
 
     public Book? GetBook(int id)
     {
-        return new SearchDb(db).GetBook(id);
+        return new DbSearchService(db).GetBook(id);
+    }
+    
+    public BookInventory? GetBookInventory(Book book)
+    {
+        return new DbSearchService(db).GetBookInventory(book);
     }
 
     public List<Book> GetAllBooks()
     {
-        return new SearchDb(db).GetAllBooks();
+        return new DbSearchService(db).GetAllBooks();
     }
 
     public List<BorrowedBook> GetBorrowedBooks(User reader)
     {
-        return new SearchDb(db).GetBorrowedBooks(reader);
+        return new DbSearchService(db).GetBorrowedBooks(reader);
     }
 
     public List<ReservedBook> GetReservedBooks(User reader)
     {
-        return new SearchDb(db).GetReservedBooks(reader);
+        return new DbSearchService(db).GetReservedBooks(reader);
     }
 
     public ReservedBook? GetReservedBook(User reader, Book book)
     {
-        return new SearchDb(db).GetReservedBook(reader, book);
+        return new DbSearchService(db).GetReservedBook(reader, book);
     }
 
-    public void AddBook(Book book)
+    public EOperationResult AddBook(Book book, int quantity)
     {
-        new InsertDb(db).AddBook(book);
+        return new DbInsertService(db).AddBook(book, quantity);
     }
 
-    public void AddBorrowedBook(BorrowedBook borrowedBook)
+    public EOperationResult AddBorrowedBook(User user, Book book)
     {
-        new InsertDb(db).AddBorrowedBook(borrowedBook);
+        return new DbInsertService(db).AddBorrowedBook(user, book);
+    }
+    
+    public EOperationResult ChangeReservedToBorrowed(User user, ReservedBook reservedBook)
+    {
+        return new DbInsertService(db).ChangeReservedToBorrowed(user, reservedBook);
     }
 
-    public void AddReservedBook(ReservedBook reservedBook)
+    public EOperationResult AddReservedBook(User user, Book book)
     {
-        new InsertDb(db).AddReservedBook(reservedBook);
+        return new DbInsertService(db).AddReservedBook(user, book);
     }
 
     public void RemoveUser(string id)
     {
-        new RemoveDb(db).RemoveUser(id);
+        new DbRemoveService(db).RemoveUser(id);
     }
 
     public void RemoveBook(int id)
     {
-        new RemoveDb(db).RemoveBook(id);
+        new DbRemoveService(db).RemoveBook(id);
     }
 
     public void RemoveBorrowedBook(BorrowedBook borrowedBook)
     {
-        new RemoveDb(db).RemoveBorrowedBook(borrowedBook);
+        new DbRemoveService(db).RemoveBorrowedBook(borrowedBook);
     }
 
     public void RemoveReservedBook(ReservedBook reservedBook)
     {
-        new RemoveDb(db).RemoveReservedBook(reservedBook);
+        new DbRemoveService(db).RemoveReservedBook(reservedBook);
     }
 
     public void SaveChanges()
     {
-        new UpdateDb(db).SaveChanges();
+        new DbUpdateService(db).SaveChanges();
     }
 
     public void PostponeBorrowedBook(BorrowedBook borrowedBook)
     {
-        new UpdateDb(db).PostponeBorrowedBook(borrowedBook);
+        new DbUpdateService(db).PostponeBorrowedBook(borrowedBook);
     }
 }
