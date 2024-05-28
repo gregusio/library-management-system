d6a75ab851025bc03f719f42da36879a0cca1b59
Grzegorz Dynak <77176069+gregusio@users.noreply.github.com>
2024-05-28 23:15:44 +0200

    Reviewing system (#87)
    * add model
    
    * update db context
    
    * create methods for user rating
    
    * make rating component interactive and static
    
    * add component to rate book
    
    * add reviews and average rating

---
+++ library-management-system/Model/UsersBookRating.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace library_management_system.Model;
+
+public class UsersBookRating
+{
+    [Key] public int Id { get; set; }
+    
+    [Required] 
+    [StringLength(30)]
+    public string? UserId { get; set; }
+    
+    [Required] public User? User { get; set; }
+    
+    [Required] public int BookId { get; set; }
+    
+    [Required] public int Rating { get; set; }
+}
